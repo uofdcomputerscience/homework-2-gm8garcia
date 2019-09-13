e40ce8c5cd9b7283ad8e2ad5a62cb398f9d52ba6
gm8garcia <32115780+gm8garcia@users.noreply.github.com>
2019-09-13 10:24:38 -0500

    Update ColorChoice.swift

--- SecondProject/ColorChoice.swift
+++ SecondProject/ColorChoice.swift
@@ -11,11 +11,15 @@ import UIKit
 enum ColorChoice {
     case blue
     case green
+    case purple
+    case yellow
     
     func displayColor() -> UIColor {
         switch self {
         case .blue: return UIColor.blue
         case .green: return UIColor.green
+        case .purple: return UIColor.purple
+        case .yellow: return UIColor.yellow
         }
     }
 }
